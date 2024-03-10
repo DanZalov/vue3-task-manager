8dd30dcae3954a3759a9a01c3f0c65dbff1e8cf8
DanZalov <danzalov@gmail.com>
2024-03-10 21:20:43 +0400

    persist state in local storage

--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -7,22 +7,29 @@ export interface Task {
 
 export const useTaskStore = defineStore('task', {
   state: (): { tasks: Task[] } => ({
-    tasks: []
+    tasks: JSON.parse(localStorage.getItem('tasks') || '[]') || []
   }),
   actions: {
+    persistToLocalStorage() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    },
     addTask(task: Task) {
       this.tasks.push(task)
+      this.persistToLocalStorage()
     },
     removeTask(id: string) {
       this.tasks = this.tasks.filter((task) => task.id !== id)
+      this.persistToLocalStorage()
     },
     updateTask(updatedTask: Task) {
       const index = this.tasks.findIndex((task) => task.id === updatedTask.id)
       if (index !== -1) {
         this.tasks[index] = updatedTask
       }
+      this.persistToLocalStorage()
     },
     getTaskById(id: string) {
+      this.persistToLocalStorage()
       return this.tasks.find((task) => task.id === id)
     }
   }
