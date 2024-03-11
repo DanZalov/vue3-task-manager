87410dcbbd7066203c100f2cd8347cb14735f50f
DanZalov <danzalov@gmail.com>
2024-03-11 05:16:10 +0400

    home updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,43 @@
+<script setup lang="ts">
+import VuePicture from '@/components/icons/VuePicture.vue'
+import PiniaPicture from '@/components/icons/PiniaPicture.vue'
+</script>
+
 <template>
   <h1>Всем привет!</h1>
+  <h2>
+    <div>
+      <span>Это приложение на Vue 3</span>
+      <div class="picture">
+        <VuePicture />
+      </div>
+    </div>
+    <div>
+      <span>с использованием Pinia</span>
+      <PiniaPicture />
+    </div>
+    <div class="last">"Таск-менеджер"</div>
+  </h2>
 </template>
+
+<style scoped>
+.picture {
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+}
+
+div {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.last {
+  margin-top: 20px;
+}
+
+span {
+  margin-right: 20px;
+}
+</style>
